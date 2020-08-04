17d5c681b50c3f4f526e6aea090aebf804acba42
mercador2 <mercador2@stu.otc.school.nz>
2020-08-04 12:44:26 +1200

    Functions that will be regularly used print statements Imports that the program will use

--- Rojan Last Card.py
+++ Rojan Last Card.py
@@ -1,5 +1,17 @@
-#Imports
+"""Imports"""
 from random import*
 from random import shuffle
+
+"""Basic Functions"""
+def gap():
+    print("")
+def player_print():
+    print("Your turn: ")
+def bot_one_print():
+    print("Bot One's Turn: ")
+def bot_two_print():
+    print("Bot Two's Turn: ")
+
+arrow = "> "
 
 
